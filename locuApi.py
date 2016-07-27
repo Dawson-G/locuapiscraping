3959e0ac0fa4b144069e945d552022c96851b08d
Dawson <dawsonmax10@gmail.com>
2016-07-27 17:00:21 +0000

    Init Commit

--- locuApi.py
+++ locuApi.py
@@ -2,10 +2,19 @@ import urllib2
 import json
 import locuApiKey
 
-url = 'https://api.locu.com/v1_0/venue/search/?name=restaurant&locality=Chicago&api_key=' + locuApiKey.apiKey
-json_obj = urllib2.urlopen(url)
-
-data = json.load(json_obj)
-
-for item in data['objects']:
-    print item, "\n"
+searchFor = "name=Food&locality=Chicago"
+    
+def locu_search(query):
+    searchFor = query.replace(' ', '%20')
+    url = "https://api.locu.com/v1_0/venue/search/?api_key=" + locuApiKey.apiKey + "&" + searchFor
+    json_obj = urllib2.urlopen(url)
+    
+    data = json.load(json_obj)
+    
+    returnData = data['objects']
+    
+    return returnData
+        
+        
+for item in locu_search(searchFor):
+    print item['name'], item['website_url'], "\n"
